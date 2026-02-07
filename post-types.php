96aa6387c7d3611b12f6ed2cc262b5854c195a90
ibilswidodo <91043778+ibilswidodo@users.noreply.github.com>
2026-02-07 10:28:20 +0700

    Update post-types.php

--- post-types.php
+++ post-types.php
@@ -1,48 +1,54 @@
 <?php
 add_action('init', function() {
     register_post_type('santri', array(
-        'labels' => array('name' => 'Santri', 'singular_name' => 'Santri', 'add_new' => 'Tambah Santri'),
+        'labels' => array(
+            'name' => 'Data Santri',
+            'singular_name' => 'Santri',
+            'add_new' => 'Tambahkan Data Santri',
+            'add_new_item' => 'Input Santri Baru',
+            'edit_item' => 'Edit Data Santri',
+            'all_items' => 'Semua Santri'
+        ),
         'public' => true,
-        'menu_icon' => 'dashicons-id-alt',
-        'supports' => array('title'),
+        'menu_icon' => 'dashicons-businesswoman',
+        'supports' => array('title'), // Title digunakan untuk Nama Lengkap
     ));
 });
 
 add_action('add_meta_boxes', function() {
-    add_meta_box('detail_santri', 'Data Lengkap Santri', 'pesantren_render_metabox', 'santri', 'normal', 'high');
+    add_meta_box('pes_detail', 'Profil & Identitas Santri', 'pes_render_meta', 'santri', 'normal', 'high');
 });
 
-function pesantren_render_metabox($post) {
-    $nis = get_post_meta($post->ID, '_santri_nis', true);
-    $alamat = get_post_meta($post->ID, '_santri_alamat', true);
-    $status_spp = get_post_meta($post->ID, '_santri_spp', true);
-    wp_nonce_field('pesantren_save', 'pesantren_nonce');
+function pes_render_meta($post) {
+    $nis = get_post_meta($post->ID, '_nis', true);
+    $jk = get_post_meta($post->ID, '_jk', true);
+    $alamat = get_post_meta($post->ID, '_alamat', true);
+    $spp = get_post_meta($post->ID, '_spp', true);
+    wp_nonce_field('pes_save_nonce', 'pes_nonce');
     ?>
     <table class="form-table">
-        <tr>
-            <th>NIS</th>
-            <td><input type="text" name="santri_nis" value="<?php echo esc_attr($nis); ?>" class="regular-text"></td>
-        </tr>
-        <tr>
-            <th>Alamat</th>
-            <td><textarea name="santri_alamat" class="regular-text"><?php echo esc_textarea($alamat); ?></textarea></td>
-        </tr>
-        <tr>
-            <th>Status SPP</th>
-            <td>
-                <select name="santri_spp">
-                    <option value="Belum Lunas" <?php selected($status_spp, 'Belum Lunas'); ?>>Belum Lunas</option>
-                    <option value="Lunas" <?php selected($status_spp, 'Lunas'); ?>>Lunas</option>
-                </select>
-            </td>
-        </tr>
+        <tr><th>NIS</th><td><input type="text" name="nis" value="<?php echo $nis; ?>" class="regular-text"></td></tr>
+        <tr><th>Jenis Kelamin</th><td>
+            <select name="jk">
+                <option value="L" <?php selected($jk, 'L'); ?>>Laki-laki</option>
+                <option value="P" <?php selected($jk, 'P'); ?>>Perempuan</option>
+            </select>
+        </td></tr>
+        <tr><th>Alamat Domisili</th><td><textarea name="alamat" class="regular-text"><?php echo $alamat; ?></textarea></td></tr>
+        <tr><th>Status Keuangan (Bulan Ini)</th><td>
+            <select name="spp">
+                <option value="Lunas" <?php selected($spp, 'Lunas'); ?>>Lunas</option>
+                <option value="Belum" <?php selected($spp, 'Belum'); ?>>Belum Lunas</option>
+            </select>
+        </td></tr>
     </table>
     <?php
 }
 
 add_action('save_post', function($post_id) {
-    if (!isset($_POST['pesantren_nonce']) || !wp_verify_nonce($_POST['pesantren_nonce'], 'pesantren_save')) return;
-    update_post_meta($post_id, '_santri_nis', sanitize_text_field($_POST['santri_nis']));
-    update_post_meta($post_id, '_santri_alamat', sanitize_textarea_field($_POST['santri_alamat']));
-    update_post_meta($post_id, '_santri_spp', sanitize_text_field($_POST['santri_spp']));
+    if (!isset($_POST['pes_nonce']) || !wp_verify_nonce($_POST['pes_nonce'], 'pes_save_nonce')) return;
+    update_post_meta($post_id, '_nis', sanitize_text_field($_POST['nis']));
+    update_post_meta($post_id, '_jk', sanitize_text_field($_POST['jk']));
+    update_post_meta($post_id, '_alamat', sanitize_textarea_field($_POST['alamat']));
+    update_post_meta($post_id, '_spp', sanitize_text_field($_POST['spp']));
 });
